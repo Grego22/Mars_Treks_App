61aeb4e8de90b0287cfeb466aa76cdb6d63e07eb
Greg Garville Jr <gjgarvil@gmail.com>
2017-10-17 11:28:07 -0400

    Correct class names

--- app/models/spaceport.rb
+++ app/models/spaceport.rb
@@ -1,4 +1,4 @@
 class Spaceport < ApplicationRecord
-    has_many :departed_flights, class_name: Flight, foreign_key: :departure_airport_id
-    has_many :arriving_flights, class_name: Flight, foreign_key: :destination_airport_id
+    has_many :departed_flights, class_name: "SpaceFlight", foreign_key: :departure_airport_id
+    has_many :arriving_flights, class_name: "SpaceFlight", foreign_key: :destination_airport_id
 end
