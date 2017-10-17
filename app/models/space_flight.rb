f339c5a6c901df269c3058a89c54300e6fb1af0b
Greg Garville Jr <gjgarvil@gmail.com>
2017-10-17 15:04:16 -0400

    installed faker gem

--- app/models/space_flight.rb
+++ app/models/space_flight.rb
@@ -2,4 +2,7 @@ class SpaceFlight < ApplicationRecord
     belongs_to :from_airport, class_name:Spaceport
     belongs_to :to_airport, class_name:Spaceport
 
+    def self.format_date
+      date = date.to_date
+      date.begnning_of_day..date.end_of_day
 end
